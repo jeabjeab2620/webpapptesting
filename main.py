4a57e029b442fa867b3d52b8ce84a11a42f89f2f
jeabjeab2620 <jeabjeab2620@gmail.com>
2022-08-20 05:46:07 +0800

    .

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from flask import *
 
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-app = FastAPI()
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to my website"}
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
